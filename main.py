2a9edc082c1f896fe1b710dd1753a6540546f7bd
Yogesh <yogeshmadhukumar86@gmail.com>
2026-07-03 12:54:08 +0530

    added display screen and history label

--- main.py
+++ main.py
@@ -14,4 +14,22 @@ calc_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 history_frame = tk.Frame(root, bg="#1a1a1a", width=200)
 history_frame.pack(side=tk.Right, fill=tk.BOTH)
 
+tk.Label(
+    history_frame,
+    text="HISTORY",
+    FONT=("Arial", 14, "bold"),
+    bg="#1a1a1a",
+    fg="#D4AF37"
+).pack(pady=10)
+# display screen for calculator input
+display = tk.Entry(
+    calc_frame,   
+    font=("Arial", 24,"bold"), 
+    bg="#1a1a1a", 
+    fg="#D4AF37",
+    bd=0, 
+    justify="right"
+)
+display.grid(row=0, column=0, columnspan=4, sticky="nsew", padx=10, pady=10)
+
 root.mainloop()
